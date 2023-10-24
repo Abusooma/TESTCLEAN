4fcb59b7da36a382703c00cee64df188146d12e3
Abusooma <soumahaboubacarsopra@gmail.com>
2023-10-24 13:03:25 +0100

    Ajout de la partie main

--- traitements.py
+++ traitements.py
@@ -14,3 +14,17 @@ def read_name(file_path):
 def write_clean_names(output_file, names):
     with open(output_file, "w") as f:
         return f.write("\n".join(names))
+
+
+# sourcery skip: remove-str-from-print
+BASE_DIR = Path(__file__).parent.parent
+FILE_DIR = BASE_DIR / "liste_des_noms.txt"
+OUTPUT_FILE = BASE_DIR / "list_name_cleans.txt"
+try:
+    names = read_name(FILE_DIR)
+    liste_names = [cleaner(name.strip()) for name in names]
+    output_file = write_clean_names(OUTPUT_FILE, liste_names)
+except FileNotFoundError:
+    print("Le fichier est introuvable..!")
+except Exception as e:
+    print("Une erreur est survenue..", str(e))
